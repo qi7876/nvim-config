1d07cc73d718c503286b88de8a312c4a0343c256
qi7876 <qi7876@gmail.com>
2024-11-06 01:02:50 +0800

    Fix for the resolution.

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -26,7 +26,7 @@ return {
         dashboard.section.header.opts.hl = "Error"
         dashboard.section.buttons.opts.hl = "AlphaButtons"
         dashboard.section.footer.opts.hl = "AlphaFooter"
-        dashboard.opts.layout[1].val = 12
+        dashboard.opts.layout[1].val = 8
         return dashboard
     end,
     config = function(_, dashboard)
