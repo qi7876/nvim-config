1f6a515089b47cccd51129e5a840d61a34bd3a18
qi7876 <qi7876@gmail.com>
2024-12-01 16:40:43 +0800

    Remove neovide config.

--- init.lua
+++ init.lua
@@ -1,11 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-if vim.g.neovide then
-    vim.o.guifont = "JetBrainsMonoNL NFM:h15"
-    vim.g.neovide_hide_mouse_when_typing = true
-end
-
 -- Registers copilot-chat filetype for markdown rendering
 require("render-markdown").setup({
     file_types = { "markdown", "copilot-chat" },
