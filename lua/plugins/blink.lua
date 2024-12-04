a1e2419a1c5037e359722b7f90d7188db268a97b
qi7876 <qi7876@gmail.com>
2024-12-04 18:32:28 +0800

    Fix for blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,28 +1,24 @@
 return {
     "saghen/blink.cmp",
-    lazy = false,
     opts = {
         keymap = { preset = "default" },
-        sources = {
-            completion = {
-                enabled_providers = { "lsp", "path", "snippets", "buffer" },
-            },
-        },
-        windows = {
-            autocomplete = {
+        completion = {
+            menu = {
                 border = "rounded",
                 winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpKindFunction,CursorLine:BlinkCmpMenuSelection,Search:None",
                 draw = {
                     columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
                 },
             },
-            documentation = {
+        },
+        documentation = {
+            auto_show = false,
+            window = {
                 border = "rounded",
-                auto_show = false,
             },
-            ghost_text = {
-                enabled = false,
-            },
+        },
+        ghost_text = {
+            enabled = false,
         },
     },
 }
